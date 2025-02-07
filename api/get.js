17ed6adcd937dac10d74d76ca6541f0b5e09dfca
bema1212 <bouwmanenergie@outlook.com>
2025-02-07 09:23:39 +0100

    Update get.js

--- api/get.js
+++ api/get.js
@@ -15,14 +15,20 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: "Both target1 and target2 parameters are required" });
     }
 
-    const apiUrl0 = `https://api.pdok.nl/bzk/locatieserver/search/v3_1/lookup?id=${target0}`;
+    // Extract coordinates from target2 (assumed to be in format "x,y")
+    const [x, y] = target2.split(',').map(coord => parseFloat(coord));
+
+ const apiUrl0 = `https://api.pdok.nl/bzk/locatieserver/search/v3_1/lookup?id=${target0}`;
     const apiUrl1 = `https://public.ep-online.nl/api/v5/PandEnergielabel/AdresseerbaarObject/${target1}`;
     const apiUrl2 = `https://opendata.polygonentool.nl/wfs?service=wfs&version=2.0.0&request=getfeature&typename=se:OGC_Warmtevlak,se:OGC_Elektriciteitnetbeheerdervlak,se:OGC_Gasnetbeheerdervlak,se:OGC_Telecomvlak,se:OGC_Waternetbeheerdervlak,se:OGC_Rioleringsvlakken&propertyname=name,disciplineCode&outputformat=application/json&&SRSNAME=urn:ogc:def:crs:EPSG::28992&bbox=${target3}`;
-
-     const encodedTarget1 = encodeURIComponent(target1);
+    const encodedTarget1 = encodeURIComponent(target1);
     const apiUrl5 = `https://service.pdok.nl/lv/bag/wfs/v2_0?service=wfs&version=2.0.0&request=getfeature&typeName=bag:verblijfsobject&outputformat=application/json&srsName=EPSG:4326&filter=%3Cfes:Filter%20xmlns:fes=%22http://www.opengis.net/fes/2.0%22%20xmlns:xsi=%22http://www.w3.org/2001/XMLSchema-instance%22%20xsi:schemaLocation=%22http://www.opengis.net/wfs/2.0%20http://schemas.opengis.net/wfs/2.0/wfs.xsd%22%3E%3Cfes:PropertyIsEqualTo%3E%3Cfes:PropertyName%3Eidentificatie%3C/fes:PropertyName%3E%3Cfes:Literal%3E${encodedTarget1}%3C/fes:Literal%3E%3C/fes:PropertyIsEqualTo%3E%3C/fes:Filter%3E`;
-
-    const apiUrlXML = `https://pico.geodan.nl/cgi-bin/qgis_mapserv.fcgi?DPI=120&map=/usr/lib/cgi-bin/projects/gebouw_woningtype.qgs&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&CRS=EPSG%3A28992&WIDTH=937&HEIGHT=842&LAYERS=gebouw&STYLES=&FORMAT=image%2Fjpeg&QUERY_LAYERS=gebouw&INFO_FORMAT=text/xml&I=611&J=469&FEATURE_COUNT=10&bbox=${target3}`;
+  
+    
+    const apiUrl3 = `https://service.pdok.nl/kadaster/kadastralekaart/wms/v5_0?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&QUERY_LAYERS=Perceelvlak&layers=Perceelvlak&INFO_FORMAT=application/json&FEATURE_COUNT=1&I=2&J=2&CRS=EPSG:28992&STYLES=&WIDTH=5&HEIGHT=5&BBOX=${target3}`;
+    const apiUrl4 = `https://service.pdok.nl/lv/bag/wfs/v2_0?service=WFS&version=2.0.0&request=GetFeature&propertyname=&count=200&outputFormat=json&srsName=EPSG:4326&typeName=bag:verblijfsobject&Filter=<Filter><DWithin><PropertyName>Geometry</PropertyName><gml:Point><gml:coordinates>${x},${y}</gml:coordinates></gml:Point><Distance units='m'>70</Distance></DWithin></Filter>`;
+    const apiUrl6 = `https://service.pdok.nl/lv/bag/wfs/v2_0?service=WFS&version=2.0.0&request=GetFeature&count=200&outputFormat=application/json&srsName=EPSG:4326&typeName=bag:pand&Filter=%3CFilter%3E%20%3CDWithin%3E%3CPropertyName%3EGeometry%3C/PropertyName%3E%3Cgml:Point%3E%20%3Cgml:coordinates%3E${x},${y}%3C/gml:coordinates%3E%20%3C/gml:Point%3E%3CDistance%20units=%27m%27%3E70%3C/Distance%3E%3C/DWithin%3E%3C/Filter%3E`;
+    const apiUrlXML = `https://pico.geodan.nl/cgi-bin/qgis_mapserv.fcgi?DPI=120&map=/usr/lib/cgi-bin/projects/gebouw_woningtype.qgs&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&CRS=EPSG%3A28992&WIDTH=937&HEIGHT=842&LAYERS=gebouw&STYLES=&FORMAT=image%2Fjpeg&QUERY_LAYERS=gebouw&INFO_FORMAT=text/xml&I=611&J=469&FEATURE_COUNT=10&bbox=${target3}`; 
 
     const fetchWithErrorHandling = async (url, options = {}) => {
       try {
@@ -52,40 +58,16 @@ export default async function handler(req, res) {
       return "<error>Failed to fetch XML data</error>";
     };
 
-    // Run all fetches in parallel
-    const [data0, data1, data2, data5, xmlData] = await Promise.all([
-      fetchWithErrorHandling(apiUrl0, { headers: { "Content-Type": "application/json" } }),
-      fetchWithErrorHandling(apiUrl1, {
-        headers: {
-          Authorization: process.env.AUTH_TOKEN,
-          "Content-Type": "application/json",
-        },
-      }),
-      fetchWithErrorHandling(apiUrl2, { headers: { "Content-Type": "application/json" } }),
-      fetchWithErrorHandling(apiUrl5, { headers: { "Content-Type": "application/json" } }),
-      fetchXMLWithRetry(apiUrlXML), // Fetch XML data
+    const [data0, data1, data2, data5, data3, data4, data6, xmlData] = await Promise.all([
+      fetchWithErrorHandling(apiUrl0),
+      fetchWithErrorHandling(apiUrl1, { headers: { "Authorization": process.env.AUTH_TOKEN } }),
+      fetchWithErrorHandling(apiUrl2),
+      fetchWithErrorHandling(apiUrl5),
+      fetchWithErrorHandling(apiUrl3),
+      fetchWithErrorHandling(apiUrl4),
+      fetchWithErrorHandling(apiUrl6),
+      fetchXMLWithRetry(apiUrlXML)
     ]);
-
- // Extract coordinates from target2 (assumed to be in format "x,y")
-    const [x, y] = target2.split(',').map(coord => parseFloat(coord));
-
-    const apiUrl3 = `https://service.pdok.nl/kadaster/kadastralekaart/wms/v5_0?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&QUERY_LAYERS=Perceelvlak&layers=Perceelvlak&INFO_FORMAT=application/json&FEATURE_COUNT=1&I=2&J=2&CRS=EPSG:28992&STYLES=&WIDTH=5&HEIGHT=5&BBOX=${target3}`;
-    const response3 = await fetchWithErrorHandling(apiUrl3, { headers: { 'Content-Type': 'application/json' } });
-
-    const apiUrl4 = `https://service.pdok.nl/lv/bag/wfs/v2_0?service=WFS&version=2.0.0&request=GetFeature&propertyname=&count=200&outputFormat=json&srsName=EPSG:4326&typeName=bag:verblijfsobject&Filter=<Filter><DWithin><PropertyName>Geometry</PropertyName><gml:Point><gml:coordinates>${x},${y}</gml:coordinates></gml:Point><Distance units='m'>70</Distance></DWithin></Filter>`;
-    const response4 = await fetchWithErrorHandling(apiUrl4, { headers: { 'Content-Type': 'application/json' } });
-
-    // New API URL added in parallel
-    const apiUrl6 = `https://service.pdok.nl/lv/bag/wfs/v2_0?service=WFS&version=2.0.0&request=GetFeature&count=200&outputFormat=application/json&srsName=EPSG:4326&typeName=bag:pand&Filter=%3CFilter%3E%20%3CDWithin%3E%3CPropertyName%3EGeometry%3C/PropertyName%3E%3Cgml:Point%3E%20%3Cgml:coordinates%3E${x},${y}%3C/gml:coordinates%3E%20%3C/gml:Point%3E%3CDistance%20units=%27m%27%3E70%3C/Distance%3E%3C/DWithin%3E%3C/Filter%3E`;
-    const response6 = await fetchWithErrorHandling(apiUrl6, { headers: { 'Content-Type': 'application/json' } });
-
-    if (!response3 || !response4 || !response6) {
-      return res.status(500).json({ error: "Error fetching data from the bbox or WFS API" });
-    }
-
-    const data3 = response3;
-    const data4 = response4;
-    const data6 = response6;
 
     const data4Features = data4.features || [];
 
@@ -115,64 +97,46 @@ export default async function handler(req, res) {
     }));
 
     const additionalDataFiltered = additionalData.filter(item => item !== null);
-
     const additionalDataMap = new Map();
     additionalDataFiltered.forEach(item => {
       additionalDataMap.set(item.identificatie, item);
     });
 
-   const mergedData = data4Features
-  .map(feature => {
-    const identificatie = feature.properties?.identificatie;
-    const additionalInfo = additionalDataMap.get(identificatie);
-    const pandData = data6.features.find(pand => pand.properties?.identificatie === feature.properties?.pandidentificatie);
+    const mergedData = data4Features
+      .map(feature => {
+        const identificatie = feature.properties?.identificatie;
+        const additionalInfo = additionalDataMap.get(identificatie);
+        const pandData = data6.features.find(pand => pand.properties?.identificatie === feature.properties?.pandidentificatie);
 
-    // Only include the feature if there is no error in the additional data and matching PAND
-    if (!additionalInfo || additionalInfo.error || !pandData) {
-      return null; // Skip this feature if there's an error or no additional data or matching PAND
-    }
-
-    return {
-      ...feature,
-      additionalData: additionalInfo.data, // Only include the successful data
-      additionalData2: [
-        {
-          geometry: pandData.geometry, // Add PAND geometry to additionalData2
+        if (!additionalInfo || additionalInfo.error || !pandData) {
+          return null;
         }
-      ],
-    };
-  })
-  .filter(item => item !== null); // Remove any null (error or missing) entries
 
+        return {
+          ...feature,
+          additionalData: additionalInfo.data,
+          additionalData2: [{ geometry: pandData.geometry }],
+        };
+      })
+      .filter(item => item !== null);
 
-
-
-
-    
-    // Construct the JSON response
     const combinedData = {
       LOOKUP: data0,
       EPON: data1,
       NETB: data2,
-      KADAS: data3,
       OBJECT: data5,
-      MERGED: mergedData,    };
+      KADAS: data3,
+      DATA4: data4,
+      PAND: data6,
+      XMLDATA: xmlData,
+      MERGED: mergedData
+    };
 
     // Set headers for multi-content response
     res.setHeader("Content-Type", "multipart/mixed; boundary=boundary123");
 
     res.status(200).send(
-      `--boundary123
-Content-Type: application/json
-
-${JSON.stringify(combinedData)}
-
---boundary123
-Content-Type: text/xml
-
-${xmlData}
-
---boundary123--`
+      `--boundary123\nContent-Type: application/json\n\n${JSON.stringify(combinedData)}\n\n--boundary123\nContent-Type: text/xml\n\n${xmlData}\n\n--boundary123--`
     );
   } catch (error) {
     console.error(error);
