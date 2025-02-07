0b6b51ba7631fc76c2814bda725258fcbcc07e47
bema1212 <bouwmanenergie@outlook.com>
2025-02-07 09:58:08 +0100

    Update get.js

--- api/get.js
+++ api/get.js
@@ -1,11 +1,12 @@
+
 export default async function handler(req, res) {
   try {
     // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
 
-    if (req.method === 'OPTIONS') {
+    if (req.method === "OPTIONS") {
       return res.status(200).end();
     }
 
@@ -18,10 +19,10 @@ export default async function handler(req, res) {
     const apiUrl0 = `https://api.pdok.nl/bzk/locatieserver/search/v3_1/lookup?id=${target0}`;
     const apiUrl1 = `https://public.ep-online.nl/api/v5/PandEnergielabel/AdresseerbaarObject/${target1}`;
     const apiUrl2 = `https://opendata.polygonentool.nl/wfs?service=wfs&version=2.0.0&request=getfeature&typename=se:OGC_Warmtevlak,se:OGC_Elektriciteitnetbeheerdervlak,se:OGC_Gasnetbeheerdervlak,se:OGC_Telecomvlak,se:OGC_Waternetbeheerdervlak,se:OGC_Rioleringsvlakken&propertyname=name,disciplineCode&outputformat=application/json&&SRSNAME=urn:ogc:def:crs:EPSG::28992&bbox=${target3}`;
-    const encodedTarget1 = encodeURIComponent(target1);
-    const apiUrl5 = `https://service.pdok.nl/lv/bag/wfs/v2_0?service=wfs&version=2.0.0&request=getfeature&typeName=bag:verblijfsobject&outputformat=application/json&srsName=EPSG:4326&filter=%3Cfes:Filter%20xmlns:fes=%22http://www.opengis.net/fes/2.0%22%20xmlns:xsi=%22http://www.w3.org/2001/XMLSchema-instance%22%20xsi:schemaLocation=%22http://www.opengis.net/wfs/2.0%20http://schemas.opengis.net/wfs/2.0/wfs.xsd%22%3E%3Cfes:PropertyIsEqualTo%3E%3Cfes:PropertyName%3Eidentificatie%3C/fes:PropertyName%3E%3Cfes:Literal%3E${encodedTarget1}%3C/fes:Literal%3E%3C/fes:PropertyIsEqualTo%3E%3C/fes:Filter%3E`;
-    const apiUrlXML = `https://pico.geodan.nl/cgi-bin/qgis_mapserv.fcgi?DPI=120&map=/usr/lib/cgi-bin/projects/gebouw_woningtype.qgs&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&CRS=EPSG%3A28992&WIDTH=937&HEIGHT=842&LAYERS=gebouw&STYLES=&FORMAT=image%2Fjpeg&QUERY_LAYERS=gebouw&INFO_FORMAT=text/xml&I=611&J=469&FEATURE_COUNT=10&bbox=${target3}`; 
 
+    const apiUrl5 = `https://service.pdok.nl/lv/bag/wfs/v2_0?service=wfs&version=2.0.0&request=getfeature&typeName=bag:verblijfsobject&outputformat=application/json&srsName=EPSG:4326&filter=%3Cfes:Filter%20xmlns:fes=%22http://www.opengis.net/fes/2.0%22%20xmlns:xsi=%22http://www.w3.org/2001/XMLSchema-instance%22%20xsi:schemaLocation=%22http://www.opengis.net/wfs/2.0%20http://schemas.opengis.net/wfs/2.0/wfs.xsd%22%3E%3Cfes:PropertyIsEqualTo%3E%3Cfes:PropertyName%3Eidentificatie%3C/fes:PropertyName%3E%3Cfes:Literal%3E${encodeURIComponent(target1)}%3C/fes:Literal%3E%3C/fes:PropertyIsEqualTo%3E%3C/fes:Filter%3E`;
+
+    const apiUrlXML = `https://pico.geodan.nl/cgi-bin/qgis_mapserv.fcgi?DPI=120&map=/usr/lib/cgi-bin/projects/gebouw_woningtype.qgs&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&CRS=EPSG%3A28992&WIDTH=937&HEIGHT=842&LAYERS=gebouw&STYLES=&FORMAT=image%2Fjpeg&QUERY_LAYERS=gebouw&INFO_FORMAT=text/xml&I=611&J=469&FEATURE_COUNT=10&bbox=${target3}`;
 
     const fetchWithErrorHandling = async (url, options = {}) => {
       try {
@@ -32,11 +33,11 @@ export default async function handler(req, res) {
         return await response.json();
       } catch (error) {
         console.error(`Error fetching ${url}:`, error.message);
-        return { error: "error" }; // Return an object with just "error" as the result
+        return { error: "error" };
       }
     };
 
- const fetchXMLWithRetry = async (url, retries = 2) => {
+    const fetchXMLWithRetry = async (url, retries = 2) => {
       for (let i = 0; i <= retries; i++) {
         try {
           const response = await fetch(url);
@@ -51,23 +52,21 @@ export default async function handler(req, res) {
       return "<error>Failed to fetch XML data</error>";
     };
 
-
-
-
+    // Run all fetches in parallel
     const [data0, data1, data2, data5, xmlData] = await Promise.all([
-      fetchWithErrorHandling(apiUrl0, { headers: { 'Content-Type': 'application/json' } }),
+      fetchWithErrorHandling(apiUrl0, { headers: { "Content-Type": "application/json" } }),
       fetchWithErrorHandling(apiUrl1, {
         headers: {
-          "Authorization": process.env.AUTH_TOKEN,
-          'Content-Type': 'application/json',
-        }
+          Authorization: process.env.AUTH_TOKEN,
+          "Content-Type": "application/json",
+        },
       }),
-      fetchWithErrorHandling(apiUrl2, { headers: { 'Content-Type': 'application/json' } }),
-      fetchWithErrorHandling(apiUrl5, { headers: { 'Content-Type': 'application/json' } }),
-	 fetchXMLWithRetry(apiUrlXML)
+      fetchWithErrorHandling(apiUrl2, { headers: { "Content-Type": "application/json" } }),
+      fetchWithErrorHandling(apiUrl5, { headers: { "Content-Type": "application/json" } }),
+      fetchXMLWithRetry(apiUrlXML), // Fetch XML data
     ]);
 
-    // Extract coordinates from target2 (assumed to be in format "x,y")
+// Extract coordinates from target2 (assumed to be in format "x,y")
     const [x, y] = target2.split(',').map(coord => parseFloat(coord));
 
     const apiUrl3 = `https://service.pdok.nl/kadaster/kadastralekaart/wms/v5_0?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetFeatureInfo&QUERY_LAYERS=Perceelvlak&layers=Perceelvlak&INFO_FORMAT=application/json&FEATURE_COUNT=1&I=2&J=2&CRS=EPSG:28992&STYLES=&WIDTH=5&HEIGHT=5&BBOX=${target3}`;
@@ -147,16 +146,17 @@ export default async function handler(req, res) {
 
 
 
+
+	  
+    // Construct the JSON response
     const combinedData = {
-        LOOKUP: data0,
+      LOOKUP: data0,
       EPON: data1,
       NETB: data2,
+     
+	KADAS: data3,
       OBJECT: data5,
-      KADAS: data3,
-      DATA4: data4,
-      PAND: data6,
-      XMLDATA: xmlData,
-      MERGED: mergedData
+      MERGED: mergedData, 
     };
 
     // Set headers for multi-content response
